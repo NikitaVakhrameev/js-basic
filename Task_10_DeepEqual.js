a2929b52e01bf203142c53b83364b66d86067867
NikitaVakhrameev <90961586+NikitaVakhrameev@users.noreply.github.com>
2022-06-24 12:41:52 +0300

    Update and rename MinValue_Task4.js to Task_10_DeepEqual.js

---
+++ Task_10_DeepEqual.js
@@ -0,0 +1,33 @@
+/*Оператор == сравнивает переменные объектов, проверяя, ссылаются ли они на один объект. Но иногда полезно было бы сравнить объекты по содержимому.
+
+Напишите функцию deepEqual, которая принимает два значения и возвращает true, только если это два одинаковых значения или это объекты, свойства которых имеют одинаковые значения, если их сравнивать рекурсивным вызовом deepEqual. Чтобы узнать, когда сравнивать величины через ===, а когда – объекты по содержимому, используйте оператор typeof. Если он выдаёт “object” для обеих величин, значит нужно делать глубокое сравнение. Не забудьте об одном дурацком исключении, случившемся из-за исторических причин: “typeof null” тоже возвращает “object”.
+
+var obj = {here: {is: "an"}, object: 2};
+console.log(deepEqual(obj, obj));
+// → true
+console.log(deepEqual(obj, {here: 1, object: 2}));
+// → false
+console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
+// → true
+*/
+
+function deepEqual(object1, object2) {
+  let props1 = Object.getOwnPropertyNames(object1);
+  let props2 = Object.getOwnPropertyNames(object2);
+
+  if (props1.length !== props2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < props1.length; i += 1) {
+    const prop = props1[i];
+    const bothAreObjects = typeof(object1[prop]) === 'object' && typeof(object2[prop]) === 'object';
+
+    if ((!bothAreObjects && (object1[prop] !== object2[prop]))
+    || (bothAreObjects && !deepEqual(object1[prop], object2[prop]))) {
+      return false;
+    }
+  }
+
+  return true;
+}
